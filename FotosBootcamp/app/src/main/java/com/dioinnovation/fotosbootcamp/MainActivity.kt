b4d7276158041940fee88690b85502a5d631f75a
mayconfuzita <mayconfuzita@gmail.com>
2021-03-13 22:29:41 -0300

    Code Created
    App test ok!

---
+++ FotosBootcamp/app/src/main/java/com/dioinnovation/fotosbootcamp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.dioinnovation.fotosbootcamp
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        pic_button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                } else {
+                    pickImageFromGalery()
+                }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    pickImageFromGalery()
+            }
+            else -> Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun pickImageFromGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PIC_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PIC_CODE){
+            image_view.setImageURI(data?.data)
+        }
+    }
+
+    companion object {
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PIC_CODE = 1001
+
+    }
+}
